317733975d66e1918a1a5ffb045cafbc4030af00
ShoaibAliport <112535359+ShoaibAliport@users.noreply.github.com>
2022-09-27 13:43:25 -0500

    Update deep.py

--- Conditionals/deep.py
+++ Conditionals/deep.py
@@ -1,5 +1,9 @@
 question = input('What is the Answer to the Great Question of Life, the Universe, and Everything? ')
-if '42' in question or 'forty two' in question or 'forty-two' in question:
+if '42' == question:
+    print('Yes')
+elif 'forty two' == question:
+    print('Yes')
+elif 'forty-two' == question:
     print('Yes')
 else:
     print('No')
