10a0c1f685be412329b0b40c04b478b3341f620e
ShoaibAliport <112535359+ShoaibAliport@users.noreply.github.com>
2022-09-27 19:10:17 -0500

    Update meal.py

--- Conditionals/meal.py
+++ Conditionals/meal.py
@@ -1,21 +1,21 @@
 def main():
-    request = input("what time is it? ")
-    time = convert(request)
-    if 7 <= time <=8:
-        print("breakfest time")
-    elif 12 <= time <= 13:
-        print("lunch time")
-    elif 18 <= time <= 19:
-        print("dinner time")
-    else:
-        print("eat nothing")
-    
+    ans = input("What time is it? ")
+    time = convert(ans)
+    if time >= 7 and time <= 8:
+        print('Breakfast time')
+    if time >= 12 and time <= 13:
+        print('Lunch time')
+    if time >=  18 and time <= 19:
+        print('Dinner time')
+
+
 
 def convert(time):
     hours, minutes = time.split(":")
-    minutes = int(minutes) / 60
-    final = int(hours) + int(minutes)
-    return final
+    new_minutes = float(minutes) / 60
+    return float(hours) + new_minutes
+
+
 
 if __name__ == "__main__":
     main()
